4e16b63ee804d74e896212bf899aacaf14171fd8
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2024-09-14 21:42:30 +0700

    fix: fix env file to docker-compose.yml instead of Dockerfile

--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -6,22 +6,24 @@ import { MongooseModuleOptions } from '@nestjs/mongoose';
 
 const getMongooseConfig = async (
   configService: ConfigService,
-): Promise<MongooseModuleOptions> => ({
-  uri: `mongodb+srv://${configService.get('MONGODB_CLUSTER')}`,
-  auth: {
-    username: configService.get('MONGODB_USERNAME'),
-    password: configService.get('MONGODB_PASSWORD'),
-  },
-  dbName: configService.get('MONGODB_DB_NAME'),
-  retryWrites: configService.get<boolean>('MONGODB_RETRY_WRITES'),
-  writeConcern: {
-    w:
-      configService.get('MONGODB_WRITE_CONCERN') === 'majority'
-        ? 'majority'
-        : parseInt(configService.get('MONGODB_WRITE_CONCERN'), 10),
-  },
-  appName: configService.get('MONGODB_APP_NAME'),
-});
+): Promise<MongooseModuleOptions> => {
+  return {
+    uri: `mongodb+srv://${configService.get('MONGODB_CLUSTER')}`,
+    auth: {
+      username: configService.get('MONGODB_USERNAME'),
+      password: configService.get('MONGODB_PASSWORD'),
+    },
+    dbName: configService.get('MONGODB_DB_NAME'),
+    retryWrites: configService.get<boolean>('MONGODB_RETRY_WRITES'),
+    writeConcern: {
+      w:
+        configService.get('MONGODB_WRITE_CONCERN') === 'majority'
+          ? 'majority'
+          : parseInt(configService.get('MONGODB_WRITE_CONCERN'), 10),
+    },
+    appName: configService.get('MONGODB_APP_NAME'),
+  };
+};
 
 @Module({
   imports: [
